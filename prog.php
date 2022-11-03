39356889eed4e2c24bfeed20a388b6e8b38cbe7b
Артём Грачёв <artgrch@mail.ru>
2022-11-03 09:20:39 +0300

    Добавлен робот

--- prog.php
+++ prog.php
@@ -1,4 +1,5 @@
-<?php echo 'Hello world! My name is Artem. Who are you?';
+<?php echo '/-_-\Hello world! My name is Artem. Who are you?';
 function maintain($rep, $com) {
+    $result = $rep.' - '.$com;
     return $result;
 }
